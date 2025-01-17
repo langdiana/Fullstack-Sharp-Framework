b93fb22f3e296ecadcada43af1d78148d0431dc5
langdiana <49799624+langdiana@users.noreply.github.com>
2025-01-16 21:32:41 -0500

    1

--- RealWorldSharp.Test/obj/Debug/net8.0/RealWorldSharp.Test.AssemblyInfo.cs
+++ RealWorldSharp.Test/obj/Debug/net8.0/RealWorldSharp.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealWorldSharp.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56693872f4654ddf1132fe24aea3bd24829f053")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealWorldSharp.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealWorldSharp.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
